82ca4cbfd16fc94eb1d6ee91d558c4dbeaa42e01
charlescosta1 <charlescostapvd@gmail.com>
2022-09-30 21:00:03 -0300

    Adicionado! Close #155

---
+++ Codigo/Service/CapitaniaService.cs
@@ -0,0 +1,38 @@
+﻿using Core;
+using Core.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service
+{
+    public class CapitaniaService : ICapitaniaService
+    {
+        public int Create(Capitania capitania)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int idCapitania)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Edit(Capitania capitania)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Capitania Get(int idCapitania)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Capitania> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
