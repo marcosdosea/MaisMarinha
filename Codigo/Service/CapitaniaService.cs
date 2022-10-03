755b092e3fe8445c875cda99c6d4f64478b60a53
Jorge Matheus <matheus.obossa@hotmail.com>
2022-10-02 23:43:20 -0300

    Método implementado na classe CapitaniaService no projeto Service. Close #163

--- Codigo/Service/CapitaniaService.cs
+++ Codigo/Service/CapitaniaService.cs
@@ -1,8 +1,10 @@
 ﻿using Core;
 using Core.Service;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,29 +12,54 @@ namespace Service
 {
     public class CapitaniaService : ICapitaniaService
     {
+        private readonly MaisMarinhaContext _context;
+        /// <summary>
+        /// Insere uma nova capitania na base de dados
+        /// </summary>
+        /// <param name="capitania"></param>
+        /// <returns></returns>
         public int Create(Capitania capitania)
         {
-            throw new NotImplementedException();
+            _context.Add(capitania);
+            _context.SaveChanges();
+            return capitania.Id;
         }
-
+        /// <summary>
+        /// Remove uma capitania na base de dados
+        /// </summary>
+        /// <param name="idCapitania"></param>
         public void Delete(int idCapitania)
         {
-            throw new NotImplementedException();
+            var _capitania = _context.Capitania.Find(idCapitania);
+            _context.Capitania.Remove(_capitania);
+            _context.SaveChanges();
         }
-
+        /// <summary>
+        /// Edita os dados de uma capitania na base de dados
+        /// </summary>
+        /// <param name="capitania"></param>
         public void Edit(Capitania capitania)
         {
-            throw new NotImplementedException();
+            _context.Update(capitania);
+            _context.SaveChanges();
         }
-
+        /// <summary>
+        /// Busca uma capitania na base de dados
+        /// </summary>
+        /// <param name="idCapitania"></param>
+        /// <returns></returns>
         public Capitania Get(int idCapitania)
         {
-            throw new NotImplementedException();
+            return _context.Capitania.Find(idCapitania);
         }
-
+        /// <summary>
+        /// Retorna todos os climas cadastrados na base de dados
+        /// </summary>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
         public IEnumerable<Capitania> GetAll()
         {
-            throw new NotImplementedException();
+            return _context.Capitania.AsNoTracking();
         }
     }
 }
