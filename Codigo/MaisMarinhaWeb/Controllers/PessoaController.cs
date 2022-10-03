9c3fd868c1db5b6cf7c5fe9d525126d63f662ced
carlos <carloshldj@gmail.com>
2022-10-03 00:58:27 -0300

    Criado e implementado a classe PessoaController e Adicionado o campo CEP como requerido na classe PessoaModel. Resolve #169

---
+++ Codigo/MaisMarinhaWeb/Controllers/PessoaController.cs
@@ -0,0 +1,94 @@
+﻿using AutoMapper;
+using Core;
+using Core.Service;
+using MaisMarinhaWeb.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MaisMarinhaWeb.Controllers
+{
+    public class PessoaController : Controller
+    {
+        private readonly IPessoaService _pessoaService;
+        private readonly IMapper _mapper;
+
+        public PessoaController(IPessoaService pessoaService, IMapper mapper)
+        {
+            _pessoaService = pessoaService;
+            _mapper = mapper;
+        }
+
+        // GET: PessoaController
+        public ActionResult Index()
+        {
+            var listaPessoas = _pessoaService.GetAll();
+            var listaPessoasModel = _mapper.Map<List<PessoaModel>>(listaPessoas);
+            return View(listaPessoasModel);
+        }
+
+        // GET: PessoaController/Details/5
+        public ActionResult Details(int id)
+        {
+            Pessoa pessoa = _pessoaService.Get(id);
+            PessoaModel pessoaModel = _mapper.Map<PessoaModel>(pessoa);
+            return View(pessoaModel);
+        }
+
+        // GET: PessoaController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: PessoaController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(PessoaModel pessoaModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var pessoa = _mapper.Map<Pessoa>(pessoaModel);
+                _pessoaService.Create(pessoa);
+            }
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: PessoaController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            Pessoa pessoa = _pessoaService.Get(id);
+            PessoaModel pessoaModel = _mapper.Map<PessoaModel>(pessoa);
+            return View(pessoaModel);
+        }
+
+        // POST: PessoaController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, PessoaModel pessoaModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var pessoa = _mapper.Map<Pessoa>(pessoaModel);
+                _pessoaService.Edit(pessoa);
+            }
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: PessoaController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            Pessoa pessoa = _pessoaService.Get(id);
+            PessoaModel pessoaModel = _mapper.Map<PessoaModel>(pessoa);
+            return View(pessoaModel);
+        }
+
+        // POST: PessoaController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, PessoaModel pessoaModel)
+        {
+            _pessoaService.Delete(id);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
