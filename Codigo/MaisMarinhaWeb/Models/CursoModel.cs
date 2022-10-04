ad5d546499b8140dbe23df71fa86d9190074c87e
charlescosta1 <charlescostapvd@gmail.com>
2022-10-03 23:37:53 -0300

    Arquivo adicionado. Close #181

--- Codigo/MaisMarinhaWeb/Models/CursoModel.cs
+++ Codigo/MaisMarinhaWeb/Models/CursoModel.cs
@@ -11,7 +11,7 @@ namespace MaisMarinhaWeb.Models
         [Display(Name = "Nome")]
         [DataType(DataType.Text)]
         [StringLength(100, ErrorMessage = "O campo aceita até 100 caracteres")]
-        public string Nome { get; set; }
+        public string? Nome { get; set; }
 
         [Required(ErrorMessage = "Campo Data Inicial é requerido")]
         [Display(Name = "Data Inicial")]
@@ -33,19 +33,19 @@ namespace MaisMarinhaWeb.Models
         [Display(Name = "Estado")]
         [DataType(DataType.Text)]
         [StringLength(2, ErrorMessage = "O campo aceita até 2 caracteres")]
-        public string Estado { get; set; }
+        public string? Estado { get; set; }
 
         [Required(ErrorMessage = "Campo Cidade é requerido")]
         [Display(Name = "Cidade")]
         [DataType(DataType.Text)]
         [StringLength(50, ErrorMessage = "O campo aceita até 50 caracteres")]
-        public string Cidade { get; set; }
+        public string? Cidade { get; set; }
 
         [Required(ErrorMessage = "Campo Requisitos é requerido")]
         [Display(Name = "Requisitos")]
         [DataType(DataType.Text)]
         [StringLength(200, ErrorMessage = "O campo aceita até 200 caracteres")]
-        public string Requisitos { get; set; }
+        public string? Requisitos { get; set; }
 
         [Required(ErrorMessage = "Campo Data Inicial de Inscrição é requerido")]
         [Display(Name = "Data Inicial de Inscrição")]
@@ -63,7 +63,7 @@ namespace MaisMarinhaWeb.Models
         [Display(Name = "Duração do Curso")]
         [DataType(DataType.Text)]
         [StringLength(150, ErrorMessage = "O campo aceita até 150 caracteres")]
-        public string Duracao { get; set; }
+        public string? Duracao { get; set; }
 
         public int IdCapitania { get; set; }
     }
