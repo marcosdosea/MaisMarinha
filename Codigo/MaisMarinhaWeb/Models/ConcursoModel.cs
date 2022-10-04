435757ab69fd8f271f145ea642de85c036cb45c5
Tiago Santiago <87858900+Ti4goS@users.noreply.github.com>
2022-10-03 22:41:38 -0300

    Finalizando ConcursoModel.cs

--- Codigo/MaisMarinhaWeb/Models/ConcursoModel.cs
+++ Codigo/MaisMarinhaWeb/Models/ConcursoModel.cs
@@ -56,48 +56,48 @@ namespace MaisMarinhaWeb.Models
         [Display(Name = "Escolaridade")]
         [DataType(DataType.Text)]
         [StringLength(50, ErrorMessage = "O campo aceita até 50 caracteres")]
-        public string Escolaridade { get; set; }
+        public string? Escolaridade { get; set; }
 
         [Required(ErrorMessage = "Campo Etapas é requerido")]
         [Display(Name = "Etapas")]
         [DataType(DataType.Text)]
         [StringLength(50, ErrorMessage = "O campo aceita até 50 caracteres")]
-        public string Etapas { get; set; }
+        public string? Etapas { get; set; }
 
         [Required(ErrorMessage = "Campo Área técnica é requerido")]
         [Display(Name = "Área técnica")]
         [DataType(DataType.Text)]
         [StringLength(50, ErrorMessage = "O campo aceita até 50 caracteres")]
-        public string AreaTecnica { get; set; }
+        public string? AreaTecnica { get; set; }
 
         [Required(ErrorMessage = "Campo Local Inscrição é requerido")]
         [Display(Name = "Local Inscrição")]
         [DataType(DataType.Text)]
         [StringLength(100, ErrorMessage = "O campo aceita até 100 caracteres")]
-        public string LocalInscricao { get; set; }
+        public string? LocalInscricao { get; set; }
 
         [Required(ErrorMessage = "Campo Valor Inscrição é requerido")]
         [Display(Name = "Valor Inscrição")]
         [DataType(DataType.Text)]
         [StringLength(10, ErrorMessage = "O campo aceita até 10 caracteres")]
-        public float ValorInscricao { get; set; }
+        public float? ValorInscricao { get; set; }
 
         [Required(ErrorMessage = "Campo Local Prova é requerido")]
         [Display(Name = "Local Prova")]
         [DataType(DataType.Text)]
         [StringLength(100, ErrorMessage = "O campo aceita até 100 caracteres")]
-        public string LocalProva { get; set; }
+        public string? LocalProva { get; set; }
 
         [Required(ErrorMessage = "Campo Cargo é requerido")]
         [Display(Name = "Cargo")]
         [DataType(DataType.Text)]
         [StringLength(60, ErrorMessage = "O campo aceita até 60 caracteres")]
-        public string Cargo { get; set; }
+        public string? Cargo { get; set; }
 
         [Required(ErrorMessage = "Campo Turma é requerido")]
         [Display(Name = "Turma")]
         [DataType(DataType.Text)]
         [StringLength(10, ErrorMessage = "O campo aceita até 10 caracteres")]
-        public string Turma { get; set; }
+        public string? Turma { get; set; }
     }
 }
