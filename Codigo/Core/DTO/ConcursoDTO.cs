a088f2437ebb46361f3d889e104531c5e55cc94f
Tiago Santiago <87858900+Ti4goS@users.noreply.github.com>
2022-10-21 20:25:03 -0300

    resolve #216

---
+++ Codigo/Core/DTO/ConcursoDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DTO
+{
+    public class ConcursoDTO
+    {
+        public int Id { get; set; }
+        public String? Nome { get; set; }
+        public DateTime DataInicialInscricao { get; set; }
+        public DateTime DataFinalInscricao { get; set; }
+        public DateTime DataProva { get; set; }
+        public String? Escolaridade { get; set; }
+    }
+}
