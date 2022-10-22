edf98c40706c19db7e2d0b3527be78173f5c87f6
edmarques12 <edandrade4040@gmail.com>
2022-10-21 22:28:12 -0300

    Classes adicionadas. Close #222

---
+++ Codigo/MaisMarinhaWeb/Mappers/InscricaoConcursoProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Core;
+using MaisMarinhaWeb.Models;
+
+namespace MaisMarinhaWeb.Mappers
+{
+    public class InscricaoConcursoProfile : Profile
+    {
+
+        public InscricaoConcursoProfile()
+        {
+            CreateMap<InscricaoConcursoModel, Inscricaoconcurso>().ReverseMap();
+        }
+    }
+}
