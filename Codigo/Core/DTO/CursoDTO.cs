cee13778a58390e56ac445841c40ecd671260a85
charlescosta1 <charlescostapvd@gmail.com>
2022-11-23 22:49:02 -0300

    DTO adicionado. Close #255

---
+++ Codigo/Core/DTO/CursoDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DTO
+{
+    internal class CursoDTO
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public DateTime DataInicial { get; set; }
+        public DateTime DataFim { get; set; }
+        public int QuantidadeVagas { get; set; }
+        public string Estado { get; set; }
+        public string Cidade { get; set; }
+        public DateTime DataInicioInscricao { get; set; }
+        public DateTime DataFimInscricao { get; set; }
+    }
+}
