ef9dc5b828af28f60762468f6fba8f304c573ea4
edmarques12 <edandrade4040@gmail.com>
2022-11-23 23:03:30 -0300

    DTO adicionado. close 256

---
+++ Codigo/Core/DTO/CapitaniaDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DTO
+{
+    internal class CapitaniaDTO
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Estado { get; set; }
+        public string Cidade { get; set; }
+        public string MetareaV { get; set; }
+        public DateTime HorarioInicio { get; set; }
+        public DateTime HorarioFim { get; set; }
+
+    }
+}
